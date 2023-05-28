12f68daf35f40954b710f6fedf5cb65586ea350c
Seano <sean01234@yahoo.com>
2023-05-27 21:02:53 -0400

    see comment in Home.js for why all files updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,4 +10,9 @@ function Home() {
   )
 }
 
+// note: in an effor to address an odd h1 error
+// I replaced the forked repo files with the original repo files
+// but kept the git & github files, I also updated my version of node
+// from v 17 to v 16, this seemed to address the h1 error
+
 export default Home;
